6ce21d4cf372811c19dca8a4bfc40658cc9e81e7
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-20 17:59:12 +0100

    add form structure

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -11,6 +11,25 @@ export default function MainPage() {
                     <h1 className="text-center text-black py-2">Altro che amazon!</h1>
                     <p className="text-body-tertiary fw-semibold">Scopri le migliori offerte per te.</p>
 
+                    {/* LOGIN */}
+
+                    <div className="login">
+                        <form>
+                            {/* EMAIL INPUT */}
+                            <label htmlFor="email">Email</label>
+                            <input id="email" name="email" type="email" />
+
+                            {/* PASSWORD INPUT */}
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" id="password" />
+
+                            {/* LOGIN BUTTONS */}
+                            <button>Accedi</button>
+                            <button>Sei nuovo? Registrati!</button>
+
+                        </form>
+                    </div>
+
                     {/* Socials */}
                     <ul>
                         <li><i class="bi bi-instagram"></i></li>
