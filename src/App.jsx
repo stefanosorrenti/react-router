5596f1cf9b5a717bec0f25993eab7f806c3c9ca1
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-20 12:08:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+     <h1>PROVA</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import * as bootstrap from 'bootstrap';
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
