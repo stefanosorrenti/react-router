30833f4eb937be3af656ab133aa1c6a699756266
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-20 13:06:49 +0100

    AppHeader: add navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,8 +2,22 @@ export default function AppHeader() {
 
 
     return (
-        <header>
-            HEADER
+        <header className="d-flex justify-content-center">
+            <nav className="py-3">
+
+                <ul className="nav nav-pills">
+                    <li className="nav-item">
+                        <a href="./info" className="nav-link">Chi siamo?</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="./prodotti" className="nav-link">Scopri i nostri prodotti</a>
+                    </li>
+                </ul>
+
+            </nav>
         </header>
     )
 }
